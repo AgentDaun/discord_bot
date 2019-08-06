d2424c47bafc0e43eff7eb9ca0a13b8b6316ace5
Arseny <agent47daun@gmail.com>
2019-08-06 23:44:51 +0300

    Changed API token and ID of channels

--- discordBot.py
+++ discordBot.py
@@ -17,8 +17,7 @@ HELP_TEXT = '```$online - Информация о сервереnn\n$settings - 
 COMMAND_PREFIX = "$"
 
 # Токен для бота, можешь поменять его/получить на https://discordapp.com/developers/applications/590070691634741258/bots
-# BOT_TOKEN = "NTkwMDcwNjkxNjM0NzQxMjU4.XRkZDw.DSB0RidI-Za2zcgoSAXSqba0w04"
-BOT_TOKEN = "NTk1ODk2NjMxODk1Nzg1NTI1.XUnXCw.n2_9p9Q0uWAC0d8Kv6o9dzJvfJw"
+BOT_TOKEN = "NTkwMDcwNjkxNjM0NzQxMjU4.XRkZDw.DSB0RidI-Za2zcgoSAXSqba0w04"
 
 # Название канала, показывающий онлайн на сервере. В конце подставляется сам онлайн.
 TEXT_ON_ONLINE_CHANNEL = "Сейчас в игре: "
@@ -37,10 +36,8 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-    # onlineChannel = bot.get_channel(594983928679628814)
-    # worldRankChannel = bot.get_channel(594984209907449908)
-    onlineChannel = bot.get_channel(595899795550371840)
-    worldRankChannel = bot.get_channel(595899869898473472)
+    onlineChannel = bot.get_channel(594983928679628814)
+    worldRankChannel = bot.get_channel(594984209907449908)
     while True:
         try:
             info = get_server_info()['attributes']
